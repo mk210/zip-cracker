3e9d0943c2bb8652cd236b73d28d6f65ae0cee79
Mohit Katta <mk210@hw.ac.uk>
2020-10-01 12:56:30 +0400

    debugging requirements.py

--- requirements.py
+++ requirements.py
@@ -2,10 +2,11 @@ import os
 import subprocess
 
 def sysChecks():
-    os.system("pip3 --version")
-    if SystemExit:
+    pip=os.system("pip3 --version")
+    # print(pip)
+    if pip != 0:
         print('Please install PIP3 for the software to work :))')
-        exit()
+        exit()     
     
     # if pip == '\'pip3\' is not recognized as an internal or external command,operable program or batch file.':
     #     print('Please install PIP3 for the software to work :))')
